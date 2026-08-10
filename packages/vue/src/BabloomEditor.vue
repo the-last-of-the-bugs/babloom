9a6b1006ba5d4caea7b2241fe464bac03647e7b8
ayuwidyaagata <ayuwagata@gmail.com>
2026-08-10 13:38:59 +0700

    feat(vue): initialize Vue adapter package

---
+++ packages/vue/src/BabloomEditor.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import type { EditorOptions } from "@babloom/core";
+import { useEditor } from "./useEditor";
+
+const props = defineProps<{ options?: Omit<EditorOptions, "element">; class?: string }>();
+const { editor, element } = useEditor(props.options ?? {});
+defineExpose({ editor });
+</script>
+
+<template>
+  <div ref="element" :class="props.class" />
+</template>
