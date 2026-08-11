ffced63c36fa04e1ee1b1c42151e8a3c44fdf381
ayuwidyaagata <ayuwagata@gmail.com>
2026-08-11 07:49:49 +0700

    hotfix: fix adapter package build configuration

--- packages/svelte/vite.config.ts
+++ packages/svelte/vite.config.ts
@@ -1,6 +1,21 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { fileURLToPath } from "node:url";
 
 export default defineConfig({
   plugins: [svelte()],
+
+  build: {
+    lib: {
+      entry: fileURLToPath(
+        new URL("./src/lib/index.ts", import.meta.url),
+      ),
+      formats: ["es"],
+      fileName: "index",
+    },
+
+    rollupOptions: {
+      external: ["svelte", "@babloom/core"],
+    },
+  },
 });
---
+++ packages/vue/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "BabloomVue",
+      fileName: "index",
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["vue", "@babloom/core"],
+    },
+  },
+});
