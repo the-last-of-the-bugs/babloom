dff28ea19890cf2fa5fac9de4b0fc2282974e253
ayuwidyaagata <ayuwagata@gmail.com>
2026-08-11 12:58:12 +0700

    chore: ignore generated build files

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
@@ -8,6 +9,9 @@ export default defineConfig({
     tanstackStart(),
     react(),
     tailwindcss(),
+    nitro({
+      preset: "vercel",
+    }),
   ],
 
   resolve: {
