6c41739d4809fd920e4f4006e7fced059c099c66
ayuwidyaagata <ayuwagata@gmail.com>
2026-08-11 08:30:54 +0700

    hotfix: fix duplicate TanStack Router code splitting

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
-    tanstackRouter({
-      target: "react",
-      autoCodeSplitting: true,
-    }),
     react(),
     tailwindcss(),
   ],
